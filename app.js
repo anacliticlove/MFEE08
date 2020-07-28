8189d35dccfa19b942dfdb548e93289f2c1e8d8e
Vincent Huang <anacliticlove@gmail.com>
2020-07-28 18:48:04 +0800

    finish guideList database information get

--- app.js
+++ app.js
@@ -1,41 +1,60 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mysql = require("mysql");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const { response } = require("express");
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+//連接資料庫
+var conn = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  password: "",
+  database: "mfee08",
+});
+app.get("/guide/123", function (request, response) {
+  conn.query("select * from tripList", "", function (err, rows) {
+    if (err) {
+      console.log(JSON.stringify(err));
+      return;
+    }
+    console.log(JSON.stringify(rows));
+    response.send(JSON.stringify(rows));
+  });
+});
 
-app.use(logger('dev'));
+//view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
+var mysql = require("mysql");
+app = express()
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
-router.get('/guide', function(req, res, next) {
-  res.render('guide_list', { title: 'Express' });
-});
+
+router.get("/guide", function (req, res, next) {
+  res.render("guide_list", {
+    title:"21",
+    tripPrice: "300",
+    tripName: "台中",
+  });})
+
+
+
+
 
 module.exports = router;
