0502ef90ab62a053e40e2b3ebda4d4ffe239ca96
Vincent Huang <anacliticlove@gmail.com>
2020-07-30 16:24:27 +0800

    add readme

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,18 @@
 var express = require("express");
 var router = express.Router();
 var mysql = require("mysql");
-app = express()
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
 router.get("/guide", function (req, res, next) {
+  res.render("guide_list", {
+    title:"21",
+    tripPrice: "300",
+    tripName: "台中",
+  });})
+router.get("/trip", function (req, res, next) {
   res.render("guide_list", {
     title:"21",
     tripPrice: "300",
